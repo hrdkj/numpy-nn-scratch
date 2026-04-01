09ceabf84fabb883b2d8242becf01492f5630d5d
hrdkdev <jhardik607@gmail.com>
2026-04-01 18:41:59 +0530

    generalisation for n weights and inputs

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import math
+
+
 def main():
-    print("Hello from numpy-nn-scratch!")
-    w1 = float(input())
-    w2 = float(input())
-    w3 = float(input())
-    age = int(input("enter your age-"))
-    weight = float(input("Enter your weight-"))
-    sugar = int(input("Enter your blood sugar level-"))
-    b = float(input("Enter bias:"))
-    x1 = age / 100
-    x2 = weight / 150
-    x3 = (sugar - 70 )/ 330
-    weighted_sum = w1*x1 + w2*x2 + w3*x3 + b
-    output = 1/ (1+ math.exp(-(weighted_sum)))
+    inputs = list(map(float, (input("enter input-")).split()))
+    weights = list(map(float, (input("enter weights-")).split()))
+    bias = float(input("enter bias-"))
+    weighted_sum = bias
+    if len(weights) == len(inputs):
+        for i in range(len(weights)):
+            s = inputs[i] * weights[i]
+            weighted_sum += s
+    else:
+        raise ValueError("no. of weights and inputs are not equal")
+    output = 1 / (1 + math.exp(-(weighted_sum)))
     print(output)
 
-main()
 
+main()
