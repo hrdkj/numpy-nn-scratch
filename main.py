91e81bb6f3f30f736ce9061ce365d3ba21060808
hrdkdev <jhardik607@gmail.com>
2026-04-01 15:15:24 +0530

    bias shift

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import math
 def main():
     print("Hello from numpy-nn-scratch!")
-    a = float(input())
-    b = float(input())
-    c = float(input())
+    w1 = float(input())
+    w2 = float(input())
+    w3 = float(input())
     age = int(input("enter your age-"))
     weight = float(input("Enter your weight-"))
     sugar = int(input("Enter your blood sugar level-"))
+    b = float(input("Enter bias:"))
     x1 = age / 100
     x2 = weight / 150
     x3 = (sugar - 70 )/ 330
-    weighted_sum = a*x1 + b*x2 + c*x3
+    weighted_sum = w1*x1 + w2*x2 + w3*x3 + b
     output = 1/ (1+ math.exp(-(weighted_sum)))
     print(output)
 
