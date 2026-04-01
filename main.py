68e7b3b07496c0394190f6e5126789149d321409
hrdkdev <jhardik607@gmail.com>
2026-04-01 20:39:04 +0530

    refactor: replace loop with np.dot and numpy arrays

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import math
 
+import numpy as np
+
 
 def main():
-    inputs = list(map(float, (input("enter input-")).split()))
-    weights = list(map(float, (input("enter weights-")).split()))
+    inputs = np.array(list(map(float, input("enter inputs-").split())))
+    weights = np.array(list(map(float, input("enter weights-").split())))
     bias = float(input("enter bias-"))
-    weighted_sum = bias
     if len(weights) == len(inputs):
-        for i in range(len(weights)):
-            s = inputs[i] * weights[i]
-            weighted_sum += s
+        s = np.dot(inputs,weights)
+        weighted_sum = s + bias
     else:
         raise ValueError("no. of weights and inputs are not equal")
     output = 1 / (1 + math.exp(-(weighted_sum)))
