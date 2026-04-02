8f5a9089f10c62130162b2dde70ca57f7a02cd53
hrdkdev <jhardik607@gmail.com>
2026-04-02 09:09:06 +0530

    multiple neurons with multiple weights

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ import numpy as np
 
 def main():
     inputs = np.array(list(map(float, input("enter inputs-").split())))
-    weights = np.array(list(map(float, input("enter weights-").split())))
+    weights = np.array(list(map(float, input("enter weights-").split()))).reshape(4,3)
     bias = float(input("enter bias-"))
-    if len(weights) == len(inputs):
-        s = np.dot(inputs,weights)
+    if weights.shape[1] == len(inputs):
+        s = np.dot(weights, inputs)
         weighted_sum = s + bias
     else:
         raise ValueError("no. of weights and inputs are not equal")
-    output = 1 / (1 + math.exp(-(weighted_sum)))
+    output = 1 / (1 + np.exp(-(weighted_sum)))
     print(output)
 
 
