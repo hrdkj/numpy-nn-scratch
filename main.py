74ec2979314457f71cd109bec4533553c2bfce15
hrdkdev <jhardik607@gmail.com>
2026-04-02 09:13:10 +0530

    add shape assertion

--- main.py
+++ main.py
@@ -7,11 +7,9 @@ def main():
     inputs = np.array(list(map(float, input("enter inputs-").split())))
     weights = np.array(list(map(float, input("enter weights-").split()))).reshape(4,3)
     bias = float(input("enter bias-"))
-    if weights.shape[1] == len(inputs):
-        s = np.dot(weights, inputs)
-        weighted_sum = s + bias
-    else:
-        raise ValueError("no. of weights and inputs are not equal")
+    assert weights.shape[1] == len(inputs), "shape mismatch"
+    s = np.dot(weights, inputs)
+    weighted_sum = s + bias
     output = 1 / (1 + np.exp(-(weighted_sum)))
     print(output)
 
